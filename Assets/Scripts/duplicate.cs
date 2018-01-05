7dda405dc0e07f44cb8cbed4947e0dfd3c1e2acf
Elise Kocik <elise.kocik@utbm.fr>
2018-01-05 19:09:50 +0100

    Pump : oil and beer

---
+++ Assets/Scripts/duplicate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class duplicate : MonoBehaviour {
+	private float timer;
+	// Use this for initialization
+	void Start () {
+		timer = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer > 4.0f) {
+			//Debug.Log("duplicate");
+			for (int i = 0; i < 3; i++) {
+				GameObject drop = (GameObject)Instantiate (gameObject, this.transform.position, Quaternion.identity);
+				drop.GetComponent<duplicate> ().enabled = false;
+			}
+			this.enabled = false;
+		}
+		timer += Time.deltaTime;
+
+	}
+}
---
+++ Assets/Scripts/spawn.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour {
+	public GameObject dropPrefab;
+	private bool spawning =false;
+	private float timer;
+	private bool stop = true;
+	private GameObject Pivot;
+	public Animator Pivot_anim;
+	public GameObject spawner;
+
+	// Use this for initialization
+	void Start () {
+		timer = 0.0f;
+		Pivot_anim.SetBool ("Down", false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.A)) {
+			if (Pivot_anim) {
+				Debug.Log ("haha");
+				Pivot_anim.SetBool ("Down", !Pivot_anim.GetBool ("Down"));
+			}
+			stop = !stop;
+		}
+
+		if (!spawning && !stop) {
+			if (timer > 0.04f) {
+				Water ();
+				timer = 0.0f;
+			} else if (!spawning) {
+				timer += Time.deltaTime;
+			}
+		}
+	}
+
+
+	void Water()
+	{
+		
+		//set spawning to true, to stop timer counting in the Update function
+		spawning = true;
+
+
+		GameObject drop = (GameObject)Instantiate(dropPrefab, spawner.transform.position, Quaternion.identity);
+		//drop.AddComponent<DeleteDrop>();
+		float rnd=Random.Range(0.002f,0.03f);
+		drop.transform.localScale=new Vector3(rnd,rnd,rnd);
+
+		//set spawning back to false so timer may start again
+		spawning = false;
+
+	}
+}
