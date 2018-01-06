8ee7ee1347783feaf6b2368f6553180ec20f5f75
Léon Victor <levi486@gmail.com>
2018-01-06 16:46:54 +0100

    First try at snapping position on counter

---
+++ Assets/SnapToClientOrderPosition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapToClientOrderPosition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider C)
+    {
+        Debug.Log(C.gameObject.tag);
+        if (C.gameObject.tag == "ClientOrderTargetPosition")
+        {
+            this.transform.rotation = Quaternion.AngleAxis(0, Vector3.up);
+            this.transform.position = new Vector3(transform.position.x, 1.357f, transform.position.z);
+        }
+    }
+}
