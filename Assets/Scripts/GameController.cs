319632cead8e4dd5b2d532fe3acac62cf6124c82
Léon Victor <levi486@gmail.com>
2018-01-10 02:26:01 +0100

    Random robot spawn

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,30 +9,34 @@ public class GameController : MonoBehaviour {
     public GameObject NPC;
     public int Score;
     public Text scoreText;
+    public float spawnRate = 20;
+    private float lastSpawnTime;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         spawnNPC();
         Score = 0;
         updateScoreText();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        var nbNPC = GameObject.FindGameObjectsWithTag("NPC").Length;
-        if (nbNPC == 0)
-        {
-            spawnNPC();
-        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        spawnNPC();
 	}
 
     void spawnNPC()
     {
-        Quaternion spawnRotation = Quaternion.identity;
-        //new Vector3 spawn_pos=
-        var npc = Instantiate(NPC, spawn.transform.position, NPC.transform.rotation);
-        float rnd = Random.Range(0.3f, 0.5f);
-        npc.transform.localScale = new Vector3(rnd,rnd,rnd);
+        var nbNPC = GameObject.FindGameObjectsWithTag("NPC").Length;
+        if ((nbNPC < 3 && Time.time >= lastSpawnTime + spawnRate) || nbNPC == 0)
+        {
+            lastSpawnTime = Time.time;
+            Quaternion spawnRotation = Quaternion.identity;
+            //new Vector3 spawn_pos=
+            var npc = Instantiate(NPC, spawn.transform.position, NPC.transform.rotation);
+            float rnd = Random.Range(0.3f, 0.5f);
+            npc.transform.localScale = new Vector3(rnd, rnd, rnd);
+        }
     }
 
     void updateScoreText()
